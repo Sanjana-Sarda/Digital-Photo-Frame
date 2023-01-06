d0c0f6a2a90e014d9433e4bd4e4b7c510e943b8d
Sanjana Sarda <31695528+Sanjana-Sarda@users.noreply.github.com>
2023-01-05 17:34:19 -0800

    Create disp.py
    iteration1 works but barely

---
+++ disp.py
@@ -0,0 +1,206 @@
+from machine import Pin, SPI
+import framebuf
+import utime
+
+# Display resolution
+EPD_WIDTH       = 800
+EPD_HEIGHT      = 480
+
+RST_PIN         = 12
+DC_PIN          = 8
+CS_PIN          = 9
+BUSY_PIN        = 13
+
+class EPD_7in5(framebuf.FrameBuffer):
+    def __init__(self):
+        self.reset_pin = Pin(RST_PIN, Pin.OUT)
+        
+        self.busy_pin = Pin(BUSY_PIN, Pin.IN, Pin.PULL_UP)
+        self.cs_pin = Pin(CS_PIN, Pin.OUT)
+        self.width = EPD_WIDTH
+        self.height = EPD_HEIGHT
+        
+        self.spi = SPI(1)
+        self.spi.init(baudrate=4000_000)
+        self.dc_pin = Pin(DC_PIN, Pin.OUT)
+        
+        self.buffer = bytearray(self.height * self.width // 8)
+        super().__init__(self.buffer, self.width, self.height, framebuf.MONO_HLSB)
+        self.init()
+
+    def digital_write(self, pin, value):
+        pin.value(value)
+
+    def digital_read(self, pin):
+        return pin.value()
+
+    def delay_ms(self, delaytime):
+        utime.sleep(delaytime / 1000.0)
+
+    def spi_writebyte(self, data):
+        self.spi.write(bytearray(data))
+
+    def module_exit(self):
+        self.digital_write(self.reset_pin, 0)
+
+    # Hardware reset
+    def reset(self):
+        self.digital_write(self.reset_pin, 1)
+        self.delay_ms(50) 
+        self.digital_write(self.reset_pin, 0)
+        self.delay_ms(2)
+        self.digital_write(self.reset_pin, 1)
+        self.delay_ms(50)   
+
+    def send_command(self, command):
+        self.digital_write(self.dc_pin, 0)
+        self.digital_write(self.cs_pin, 0)
+        self.spi_writebyte([command])
+        self.digital_write(self.cs_pin, 1)
+
+    def send_data(self, data):
+        self.digital_write(self.dc_pin, 1)
+        self.digital_write(self.cs_pin, 0)
+        self.spi_writebyte([data])
+        self.digital_write(self.cs_pin, 1)
+
+    def WaitUntilIdle(self):
+        print("e-Paper busy")
+        while(self.digital_read(self.busy_pin) == 0):    # Wait until the busy_pin goes LOW
+            self.send_command(0x71)
+            self.delay_ms(20)
+        self.delay_ms(20) 
+        print("e-Paper busy release")  
+
+    def TurnOnDisplay(self):
+        self.send_command(0x12) # DISPLAY REFRESH
+        self.delay_ms(100)      #!!!The delay here is necessary, 200uS at least!!!
+        self.WaitUntilIdle()
+        
+    def init(self):
+        # EPD hardware init start     
+        self.reset()
+        
+        self.send_command(0x01)  # POWER SETTING
+        self.send_data(0x07)
+        self.send_data(0x07)     # VGH=20V,VGL=-20V
+        self.send_data(0x3f)     # VDH=15V
+        self.send_data(0x3f)     # VDL=-15V
+        
+        self.send_command(0x04)  # POWER ON
+        self.delay_ms(100)
+        self.WaitUntilIdle()
+
+        self.send_command(0X00)   # PANNEL SETTING
+        self.send_data(0x1F)      # KW-3f   KWR-2F	BWROTP 0f	BWOTP 1f
+
+        self.send_command(0x61)     # tres
+        self.send_data(0x03)     # source 800
+        self.send_data(0x20)
+        self.send_data(0x01)     # gate 480
+        self.send_data(0xE0)
+
+        self.send_command(0X15)
+        self.send_data(0x00)
+
+        self.send_command(0X50)     # VCOM AND DATA INTERVAL SETTING
+        self.send_data(0x10)
+        self.send_data(0x00)
+
+        self.send_command(0X60)     # TCON SETTING
+        self.send_data(0x22)
+
+        self.send_command(0x65)     # Resolution setting
+        self.send_data(0x00)
+        self.send_data(0x00)     # 800*480
+        self.send_data(0x00)
+        self.send_data(0x00)
+        
+        return 0;
+
+    def Clear(self):
+        
+        high = self.height
+        if( self.width % 8 == 0) :
+            wide =  self.width // 8
+        else :
+            wide =  self.width // 8 + 1
+        
+        self.send_command(0x10) 
+        for j in range(0, high):
+            for i in range(0, wide):
+                self.send_data(0xff)
+                
+        self.send_command(0x13) 
+        for j in range(0, high):
+            for i in range(0, wide):
+                self.send_data(0x00)
+                
+        self.TurnOnDisplay()
+        
+    def ClearBlack(self):
+        
+        high = self.height
+        if( self.width % 8 == 0) :
+            wide =  self.width // 8
+        else :
+            wide =  self.width // 8 + 1
+        
+        self.send_command(0x10) 
+        for j in range(0, high):
+            for i in range(0, wide):
+                self.send_data(0x00)
+                
+        self.send_command(0x13) 
+        for j in range(0, high):
+            for i in range(0, wide):
+                self.send_data(0xff)
+                
+        self.TurnOnDisplay()
+        
+    def display(self,blackimage):
+        
+        high = self.height
+        if( self.width % 8 == 0) :
+            wide =  self.width // 8
+        else :
+            wide =  self.width // 8 + 1
+        
+        self.send_command(0x10) 
+        for j in range(0, high):
+            for i in range(0, wide):
+                self.send_data(blackimage[i + j * wide])
+                
+        self.send_command(0x13) 
+        for j in range(0, high):
+            for i in range(0, wide):
+                self.send_data(blackimage[i + j * wide])
+                
+        self.TurnOnDisplay()
+
+
+    def sleep(self):
+        self.send_command(0x02) # power off
+        self.WaitUntilIdle()
+        self.send_command(0x07) # deep sleep
+        self.send_data(0xa5)
+
+if __name__=='__main__':
+    epd = EPD_7in5()
+    epd.Clear()
+    
+    with open('small.pbm', 'rb') as f:
+        f.readline() 
+        f.readline() # Creator comment
+        f.readline() # Dimensions
+        data = bytearray(f.read())
+    fbuf = framebuf.FrameBuffer(data, 128, 64, framebuf.MONO_HLSB)
+    epd.blit(fbuf, 0, 0)
+    epd.display(epd.buffer)    
+    epd.delay_ms(500)
+        
+    epd.Clear()
+    epd.delay_ms(2000)
+    print("sleep")
+    epd.sleep()
+
